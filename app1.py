7e38019249876b8e336fc5a2ab0f0a8b6f8b100e
Mayank <myk.vit@gmail.com>
2019-06-14 22:33:21 -0600

    refactor code

--- app1.py
+++ app1.py
@@ -18,44 +18,48 @@ api = Api(app)
 jwt = JWT(app, authenticate, identity)
 
 class Item(Resource):
-
+    parser = reqparse.RequestParser()
+    parser.add_argument("price", type = int,required= True, help ="the field cannot be left blank" )
     # here name of item will be given in the url similarly for post
-    # @jwt_required()
-    def get(self, name):
+
+    # find if name already exists
+    def findByName(self,name):
         conn = sqlite3.connect('database.db')
         cur = conn.cursor()
         query = cur.execute('select * from items where name = ?',(name,))
         res = query.fetchone()
         cur.close()
+        return res
+    # insert into items TABLE
+    def insert(self,name,priceValue):
+        conn = sqlite3.connect('database.db')
+        cur = conn.cursor()
+        cur.execute('insert into items (name, price) values(?, ?)',(name,priceValue['price']))
+        conn.commit()
+        cur.close()
+        return {"message":"item updated"},201
+
+    # @jwt_required()
+    def get(self, name):
+        res = self.findByName(name)
         if res:
             return {"name":name,"price":res[2]},200
         else:
             return {"item":"item not found"},404
 
-
     def post(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("price", type = int,required= True, help ="the field cannot be left blank" )
-        args = parser.parse_args()
-        conn = sqlite3.connect('database.db')
-        cur = conn.cursor()
-        query = cur.execute('select * from items where name = ?',(name,))
-        res = query.fetchone()
+        args = self.parser.parse_args()
+        res = self.findByName(name)
         if res:
-            cur.close()
             return {"message":"bad request item already present"},400
-        cur.execute('insert into items (name, price) values(?, ?)',(name,args['price']))
-        conn.commit()
-        cur.close()
+        self.insert(name,args)
         return {'name':name,'price':args['price']},201
 
-
     def delete(self, name):
-        conn = sqlite3.connect('database.db')
-        cur = conn.cursor()
-        query = cur.execute('select * from items where name = ?',(name,))
-        res = query.fetchone()
+        res = self.findByName(name)
         if res:
+            conn = sqlite3.connect('database.db')
+            cur = conn.cursor()
             cur.execute('delete from items where name = ?',(name,))
             conn.commit()
             cur.close()
@@ -64,38 +68,32 @@ class Item(Resource):
         return {'message':'you entered incorrect name'},400
 # no matter how many times you call put function the output never changes
     def put(self,name):
-        parser = reqparse.RequestParser()
-        parser.add_argument("price", type = int,required= True, help ="the field cannot be left blank" )
-        args = parser.parse_args()
-        conn = sqlite3.connect('database.db')
-        cur = conn.cursor()
-        query = cur.execute('select * from items where name = ?',(name,))
-        res = query.fetchone()
+        args = self.parser.parse_args()
+        res = self.findByName(name)
         if res:
+            conn = sqlite3.connect('database.db')
+            cur = conn.cursor()
             cur.execute('update items SET price = ? WHERE name = ?',(args['price'],name));
             conn.commit()
             cur.close()
             return {"message":"item updated"},201
         #insert
-        cur.execute('insert into items (name, price) values(?, ?)',(name,args['price']))
-        conn.commit()
-        cur.close()
+        self.insert(name,args)
         return {"message":"item updated"},201
 
 class ItemList(Resource):
     def get(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("price", type = int,required= True, help ="the field cannot be left blank" )
-        args = parser.parse_args()
         conn = sqlite3.connect('database.db')
         cur = conn.cursor()
         query = cur.execute('select * from items')
-        res = query.fetchone()
+        items = query.fetchall()
         cur.close()
-        if res:
-            return
-
-
+        itemsList = []
+        if items:
+            for item in items:
+                itemsList.append({"name":item[1],"price":item[2]})
+            return itemsList
+        return {"message":"database is empty"}
 #end points
 api.add_resource(Item,'/item/<string:name>')
 api.add_resource(ItemList,'/items')
